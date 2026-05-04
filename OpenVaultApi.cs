ca7f0aafe6788133b53d4b52fa998cf481f30ed1
Denys Orel <4uzhoy@gmail.com>
2026-05-04 15:27:30 +0200

    fix links

--- OpenVaultApi.cs
+++ OpenVaultApi.cs
@@ -3,108 +3,107 @@ using System;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
-namespace CADShark.Common.DBManager
+namespace CADShark.Common.DBManager;
+
+public class OpenVaultApi
 {
-    public class OpenVaultApi
+    private readonly HttpClient _client;
+
+    public OpenVaultApi()
     {
-        private readonly HttpClient _client;
-
-        public OpenVaultApi()
+        var handler = new HttpClientHandler
         {
-            var handler = new HttpClientHandler
-            {
-                ServerCertificateCustomValidationCallback = (msg, cert, chain, errors) => true
-            };
+            ServerCertificateCustomValidationCallback = (msg, cert, chain, errors) => true
+        };
 
-            _client = new HttpClient(handler)
-            {
-                BaseAddress = new Uri("https://192.168.1.109:443/")
-            };
-        }
-
-        public async Task<int> CreateObjectAsync(int objectType)
+        _client = new HttpClient(handler)
         {
-            const string url = "api/objects";
+            BaseAddress = new Uri("https://192.168.1.109:443/")
+        };
+    }
 
-            var request = new CreateObjectRequest
-            {
-                ObjectType = objectType
-            };
+    public async Task<int> CreateObjectAsync(int objectType)
+    {
+        const string url = "api/objects";
 
-            var json = JsonConvert.SerializeObject(request);
-            var content = new StringContent(json, Encoding.UTF8, "application/json");
-
-            var response = await _client.PostAsync(url, content);
-            response.EnsureSuccessStatusCode();
-
-            var resultJson = await response.Content.ReadAsStringAsync();
-
-            var result = JsonConvert.DeserializeObject<ApiResponse<CreateObjectResponse>>(resultJson);
-
-            return result.Data.ObjectId;
-        }
-
-        // ========= SEARCH OBJECTS =========
-        public async Task<int[]> SearchObjectsAsync(SearchRequest request)
+        var request = new CreateObjectRequest
         {
-            const string url = "api/objects/search";
+            ObjectType = objectType
+        };
 
-            var json = JsonConvert.SerializeObject(request);
-            var content = new StringContent(json, Encoding.UTF8, "application/json");
+        var json = JsonConvert.SerializeObject(request);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
 
-            var response = await _client.PostAsync(url, content);
-            response.EnsureSuccessStatusCode();
+        var response = await _client.PostAsync(url, content);
+        response.EnsureSuccessStatusCode();
 
-            var resultJson = await response.Content.ReadAsStringAsync();
+        var resultJson = await response.Content.ReadAsStringAsync();
 
-            var result = JsonConvert.DeserializeObject<ApiResponse<SearchResponse>>(resultJson);
+        var result = JsonConvert.DeserializeObject<ApiResponse<CreateObjectResponse>>(resultJson);
 
-            return result?.Data?.ObjectIds ?? [];
-        }
+        return result.Data.ObjectId;
+    }
 
-        // ========= CREATE ATTRIBUTE =========
-        public async Task<string> AddAttribute(int objectId, int attributeId, string value)
+    // ========= SEARCH OBJECTS =========
+    public async Task<int[]> SearchObjectsAsync(SearchRequest request)
+    {
+        const string url = "api/objects/search";
+
+        var json = JsonConvert.SerializeObject(request);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        var response = await _client.PostAsync(url, content);
+        response.EnsureSuccessStatusCode();
+
+        var resultJson = await response.Content.ReadAsStringAsync();
+
+        var result = JsonConvert.DeserializeObject<ApiResponse<SearchResponse>>(resultJson);
+
+        return result?.Data?.ObjectIds ?? [];
+    }
+
+    // ========= CREATE ATTRIBUTE =========
+    public async Task<string> AddAttribute(int objectId, int attributeId, string value)
+    {
+        var url = $"api/objects/{objectId}/attributes";
+
+        var request = new AttributeRequest
         {
-            var url = $"api/objects/{objectId}/attributes";
+            AttributeId = attributeId,
 
-            var request = new AttributeRequest
-            {
-                AttributeId = attributeId,
+            StringValue = value
+        };
 
-                StringValue = value
-            };
+        var json = JsonConvert.SerializeObject(request);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
 
-            var json = JsonConvert.SerializeObject(request);
-            var content = new StringContent(json, Encoding.UTF8, "application/json");
+        var response = await _client.PostAsync(url, content);
+        response.EnsureSuccessStatusCode();
 
-            var response = await _client.PostAsync(url, content);
-            response.EnsureSuccessStatusCode();
+        return await response.Content.ReadAsStringAsync();
+    }
 
-            return await response.Content.ReadAsStringAsync();
-        }
+    public async Task<string> WritteBlob(string fileName, byte[] fileBody, int objectLinkId, int attributeId,
+        int linkType)
+    {
+        var url = "api/storage";
 
-        public async Task<string> WritteBlob(string fileName, byte[] fileBody, int objectLinkId, int attributeId, int linkType)
+        var request = new StorageRequest
         {
-            var url = "api/storage";
+            FileName = fileName,
+            FileBody = Convert.ToBase64String(fileBody),
+            ObjectLinkId = objectLinkId,
+            AttributeId = attributeId,
+            LinkType = linkType
+        };
 
-            var request = new StorageRequest
-            {
-                FileName = fileName,
-                FileBody = Convert.ToBase64String(fileBody),
-                ObjectLinkId = objectLinkId,
-                AttributeId = attributeId,
-                LinkType = linkType
-            };
+        var json = JsonConvert.SerializeObject(request);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
 
-            var json = JsonConvert.SerializeObject(request);
-            var content = new StringContent(json, Encoding.UTF8, "application/json");
+        var response = await _client.PostAsync(url, content);
+        response.EnsureSuccessStatusCode();
 
-            var response = await _client.PostAsync(url, content);
-            response.EnsureSuccessStatusCode();
-
-            return await response.Content.ReadAsStringAsync();
-        }
+        return await response.Content.ReadAsStringAsync();
     }
 }
