06b54fd62f5df60559a5b2f23a5de31a69856d36
Denys Orel <DenisOrel@users.noreply.github.com>
2026-08-13 11:46:43 +0200

    Add cancellation to HTTP serialization

--- Http/IHttpContentSerializer.cs
+++ Http/IHttpContentSerializer.cs
@@ -1,11 +1,15 @@
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace OpenVault.Client.Http
+namespace OpenManage.Client.Http
 {
     internal interface IHttpContentSerializer
     {
         HttpContent CreateContent<T>(T value);
-        Task<T> ReadAsync<T>(HttpContent content);
+
+        Task<T> ReadAsync<T>(
+            HttpContent content,
+            CancellationToken cancellationToken);
     }
 }
