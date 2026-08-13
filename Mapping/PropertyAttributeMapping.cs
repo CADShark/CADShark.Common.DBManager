19b05b4bb54b3a599b119f6ded3c475fc4d575b2
Denys Orel <DenisOrel@users.noreply.github.com>
2026-08-13 17:52:26 +0200

    Add common property mapping contract

---
+++ Mapping/PropertyAttributeMapping.cs
@@ -0,0 +1,9 @@
+namespace OpenManage.Client.Mapping
+{
+    public sealed class PropertyAttributeMapping
+    {
+        public string PropertyName { get; set; }
+
+        public int AttributeId { get; set; }
+    }
+}
