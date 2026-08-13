51a008feef24d9201fa01fba6cb7e98988f56a0b
Denys Orel <DenisOrel@users.noreply.github.com>
2026-08-13 18:50:19 +0200

    Add workspace document model

---
+++ Integration/Models/WorkspaceDocumentInfo.cs
@@ -0,0 +1,9 @@
+namespace OpenManage.Client.Integration.Models
+{
+    public sealed class WorkspaceDocumentInfo
+    {
+        public string FileName { get; set; }
+
+        public string RelativePath { get; set; }
+    }
+}
