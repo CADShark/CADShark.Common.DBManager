9b8f214f84b50f6a516f45574507989f1f9800c2
Denys Orel <DenisOrel@users.noreply.github.com>
2026-08-13 19:02:48 +0200

    Execute full SolidWorks create-only workflow

--- samples/OpenManage.SolidWorks.ConsoleStub/Program.cs
+++ samples/OpenManage.SolidWorks.ConsoleStub/Program.cs
@@ -1,8 +1,9 @@
+using OpenManage.Client;
 using OpenManage.Client.Integration;
+using OpenManage.Client.Integration.Models;
 using OpenManage.Client.Mapping;
 using OpenManage.SolidWorks.Adapter;
 using System;
-using System.Collections.Generic;
 using System.Text;
 using System.Threading;
 
@@ -15,43 +16,58 @@ namespace OpenManage.SolidWorks.ConsoleStub
         {
             Console.OutputEncoding = Encoding.UTF8;
 
+            if (args.Length == 0 || !Uri.TryCreate(args[0], UriKind.Absolute, out var apiAddress))
+            {
+                Console.Error.WriteLine(
+                    "Usage: OpenManage.SolidWorks.ConsoleStub.exe <OpenVaultApiUrl> [WorkspaceRoot]");
+                return 4;
+            }
+
+            var workspaceRoot = args.Length > 1
+                ? args[1]
+                : WindowsWorkspacePathService.DefaultWorkspaceRoot;
+
             try
             {
-                var workspaceRoot = args.Length > 0
-                    ? args[0]
-                    : WindowsWorkspacePathService.DefaultWorkspaceRoot;
-
-                var documentReader = new SolidWorksDocumentReader();
-                var document = documentReader
+                var document = new SolidWorksDocumentReader()
                     .GetActiveDocumentAsync(CancellationToken.None)
                     .GetAwaiter()
                     .GetResult();
 
-                var workspace = new WindowsWorkspacePathService(workspaceRoot);
-                var workspaceDocument = workspace.Resolve(document.FullPath);
-
-                var mapper = new EngineeringPropertyMapper();
-                var mappings = new[]
+                using (var client = new OpenManageClient(
+                    new OpenManageClientOptions
+                    {
+                        BaseAddress = apiAddress
+                    }))
                 {
-                    new PropertyAttributeMapping
-                    {
-                        PropertyName = "Обозначение",
-                        AttributeId = 9
-                    },
-                    new PropertyAttributeMapping
-                    {
-                        PropertyName = "Наименование",
-                        AttributeId = 10
-                    }
-                };
+                    var service = new CreateOnlyDocumentService(client);
+                    var result = service.CreateAsync(
+                            new CreateOnlyDocumentRequest
+                            {
+                                Document = document,
+                                WorkspaceRoot = workspaceRoot,
+                                PropertyMappings = new[]
+                                {
+                                    new PropertyAttributeMapping
+                                    {
+                                        PropertyName = "Обозначение",
+                                        AttributeId = 9
+                                    },
+                                    new PropertyAttributeMapping
+                                    {
+                                        PropertyName = "Наименование",
+                                        AttributeId = 10
+                                    }
+                                }
+                            },
+                            CancellationToken.None)
+                        .GetAwaiter()
+                        .GetResult();
 
-                var attributes = mapper.Map(
-                    document.Properties,
-                    mappings,
-                    workspaceDocument.RelativePath);
+                    PrintResult(result);
 
-                PrintDocument(document, workspace.WorkspaceRoot, workspaceDocument, attributes);
-                return 0;
+                    return result.IsSuccess ? 0 : 5;
+                }
             }
             catch (SolidWorksAdapterException exception)
             {
@@ -70,32 +86,27 @@ namespace OpenManage.SolidWorks.ConsoleStub
             }
         }
 
-        private static void PrintDocument(
-            OpenManage.Client.Integration.Models.EngineeringDocumentInfo document,
-            string workspaceRoot,
-            OpenManage.Client.Integration.Models.WorkspaceDocumentInfo workspaceDocument,
-            IReadOnlyList<OpenVaultAttributeValue> attributes)
+        private static void PrintResult(CreateOnlyDocumentResult result)
         {
-            Console.WriteLine("Active SOLIDWORKS document");
-            Console.WriteLine("  Full path:      " + document.FullPath);
-            Console.WriteLine("  Workspace:      " + workspaceRoot);
-            Console.WriteLine("  Relative path:  " + workspaceDocument.RelativePath);
-            Console.WriteLine("  File name:      " + workspaceDocument.FileName);
-            Console.WriteLine("  Document kind:  " + document.DocumentKind);
-            Console.WriteLine("  Configuration:  " + (document.Configuration ?? "<not applicable>"));
+            Console.WriteLine("CreateOnly result: " + (result.IsSuccess ? "SUCCESS" : "FAILED"));
+            Console.WriteLine("  ObjectId: " + Format(result.ObjectId));
+            Console.WriteLine("  VersionId: " + Format(result.VersionId));
+            Console.WriteLine("  Added attributes: " + result.AddedAttributeCount);
+            Console.WriteLine("  FileId: " + Format(result.FileId));
 
-            Console.WriteLine();
-            Console.WriteLine("SOLIDWORKS properties:");
-            foreach (var property in document.Properties)
-                Console.WriteLine("  " + property.Key + " = " + property.Value);
+            if (!string.IsNullOrWhiteSpace(result.ErrorMessage))
+            {
+                Console.WriteLine();
+                Console.WriteLine("Error: " + result.ErrorMessage);
+                Console.WriteLine(
+                    "Already-created server data was not deleted automatically.");
+            }
+        }
 
-            Console.WriteLine();
-            Console.WriteLine("OpenVault attributes prepared for CreateOnly:");
-            foreach (var attribute in attributes)
-                Console.WriteLine("  AttributeId " + attribute.AttributeId + " = " + attribute.Value);
-
-            Console.WriteLine();
-            Console.WriteLine("No data was sent to OpenVault.");
+        private static string Format<T>(T? value)
+            where T : struct
+        {
+            return value.HasValue ? value.Value.ToString() : "<not created>";
         }
     }
 }
