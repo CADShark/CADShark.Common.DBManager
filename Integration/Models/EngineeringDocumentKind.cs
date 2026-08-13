ca436348683bd3b46d3145432b82d0ab5bede2b7
Denys Orel <DenisOrel@users.noreply.github.com>
2026-08-13 17:52:23 +0200

    Add neutral engineering document kind

---
+++ Integration/Models/EngineeringDocumentKind.cs
@@ -0,0 +1,13 @@
+namespace OpenManage.Client.Integration.Models
+{
+    public enum EngineeringDocumentKind
+    {
+        Unknown = 0,
+        Part = 1,
+        Assembly = 2,
+        Drawing = 3,
+        Schematic = 4,
+        PrintedCircuitBoard = 5,
+        Project = 6
+    }
+}
