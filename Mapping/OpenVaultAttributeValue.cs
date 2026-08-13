cd9d44c806482e326cafacdfe12707f52a9b7c34
Denys Orel <DenisOrel@users.noreply.github.com>
2026-08-13 18:50:38 +0200

    Add mapped attribute value model

---
+++ Mapping/OpenVaultAttributeValue.cs
@@ -0,0 +1,9 @@
+namespace OpenManage.Client.Mapping
+{
+    public sealed class OpenVaultAttributeValue
+    {
+        public int AttributeId { get; set; }
+
+        public string Value { get; set; }
+    }
+}
